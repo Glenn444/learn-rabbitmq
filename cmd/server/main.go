89433c6465936edcac856e51b1f4a412938ec790
Glenn444 <gmark1586@gmail.com>
2025-11-05 14:40:43 +0300

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+    amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	
+
+	fmt.Println("Starting Peril server...")
+}
