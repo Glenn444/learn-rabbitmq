d9904dad0ffc7a4cb4194882f2b897446ddc1f56
Glenn444 <gmark1586@gmail.com>
2025-11-05 16:37:39 +0300

    Dial up connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
-    amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	connectionString := "amqp://guest:guest@localhost:5672/"
-	
+	conn, err := amqp.Dial(connectionString)
 
-	fmt.Println("Starting Peril server...")
+	if err != nil {
+		fmt.Printf("error occurred connecting to amqp: %v", err)
+	}
+	fmt.Printf("connection was successful")
+	defer conn.Close()
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+
+	fmt.Printf("signal to exit received: %v",sig)
 }
