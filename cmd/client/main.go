9a2bf84da9b45f31204e116cda2ccdcb98ea6b62
Glenn444 <gmark1586@gmail.com>
2025-11-18 12:05:47 +0300

    Connect client to rabbitmq

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("Error loading .env file")
+	}
+	
+	connectionString := os.Getenv("connectionString")
+	
+	conn,err := amqp.Dial(connectionString)
+	if err != nil{
+		log.Fatal("Error connecting to rabbitmq")
+	}
+
+	defer conn.Close()
+
+	
 	fmt.Println("Starting Peril client...")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/joho/godotenv"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	connectionString := "amqp://guest:guest@localhost:5672/"
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("Error Loading .env file")
+	}
+
+	connectionString := os.Getenv("amqpConnectionUrl")
 	conn, err := amqp.Dial(connectionString)
 
 	if err != nil {
