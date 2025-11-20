5d596a0aeeb9d5a6f24a461490f04da90651dfd9
Glenn444 <gmark1586@gmail.com>
2025-11-21 00:33:44 +0300

    Subscribe json

--- internal/pubsub/publishjson.go
+++ internal/pubsub/publishjson.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -76,4 +77,39 @@ func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queu
 	}
 	return ch, q, nil
 
+}
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueType,
+	handler func(T),
+) error {
+	ch,q,err := DeclareAndBind(conn,exchange,queueName,key,queueType)
+	if err != nil{
+		log.Fatalf("error occurred on DeclareAndBind %v\n",err)
+	}
+
+	messages, err := ch.Consume(q.Name,"",false,false,false,false,nil)
+	if err != nil{
+	log.Fatalf("messages.consume: %v",err)
+	}
+
+	
+
+	
+	go func() {
+		for message := range messages{
+			var data T
+			json.Unmarshal(message.Body, &data)
+			handler(data)
+
+			message.Ack(false)
+
+		}
+	}()
+	
+	return nil
 }
